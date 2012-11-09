122026087194c9febd41e5c76a401e6682a34991
Maiah <jjmmacariola@gmail.com>
2012-11-09 19:50:03 +0800

    Initial angularjs design.

---
+++ js/todo.js
@@ -0,0 +1,50 @@
+﻿angular.module('TodoApp', ['ngResource']);
+
+function TodoCtrl($scope, $http, $resource) {
+  $scope.todos = [];
+
+  var Todo = $resource('/api/todos');
+  Todo.get(function(result) {
+    angular.forEach(result, function(todo) {
+      $scope.todos.push(todo);
+    });
+  });
+
+  // This is also working
+  /*
+  $http.get('/api/todos')
+    .success(function(data, status) {
+      angular.forEach(data, function(todo) {
+        $scope.todos.push(todo);
+    });
+    });
+  */
+
+  $scope.addTodo = function() {
+    $scope.todos.push({ text: $scope.todoText, done: false });
+    $scope.todoText= '';
+  };
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.clearCompleted = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) {
+        $scope.todos.push(todo);
+      }
+    });
+  };
+
+  $scope.remove = function(todo) {
+    var idx = $scope.todos.indexOf(todo);
+    $scope.todos.splice(idx, 1);
+  };
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/api/todos', function(req, res){
+  var result = {
+        0: { text: 'Call nay Helen for Pearl SSS', done: false },
+        1: { text: 'Call NSO for marriage cert', done: false },
+        2: { text: 'Print Pearl Grandfather photo', done: false},
+        3: { text: 'Check computer for DBD Coop.', done: false},
+        4: { text: 'Check laptop for James', done: false},
+        5: { text: 'Check laptop for me', done: false}
+      };
+  res.json(result);
+});
+
+app.listen(7000);
+console.log('Listening on port 7000');
